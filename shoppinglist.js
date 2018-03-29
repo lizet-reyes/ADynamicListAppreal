87d65b597b371e8eaab183619e980a3609dbdd61
Yessenia Hernandez Hernandez <yh90926@srcs.loc>
2018-03-29 12:17:02 -0700

    progress

--- shoppinglist.js
+++ shoppinglist.js
@@ -1,11 +1,11 @@
-var addButton = document.getElementById('submitItem');
-var list = docuemnt.getElementById("addItem ");
+var addButton = document.getElementById('addItem');
+var list = docuemnt.getElementById("list");
 
 
 
 
 function addItem (){
-    var input = document.getElementById("Birthday").value;
+    var input = document.getElementById("Items").value;
     var item = document.createElement("li");
     item.textcontent = input;
     list.appendChild(item);
