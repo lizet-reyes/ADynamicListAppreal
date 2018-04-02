4860824788cf4bae36bcf2554196c4afb8f66429
Lizet N Reyes Rodas <lr40247@srcs.loc>
2018-04-02 11:39:01 -0700

    new changes

--- Lizet.js
+++ Lizet.js
@@ -1,6 +1,6 @@
-
-var myButton = document.getElementById('submitItem');
-myButton.addEventListener('click', displayAllOutputs);
-
-
-function delete(inputBirthday) {
+function deleteItem(){
+    var list = document.getElementById("list");
+    var input = document.getElementById("Items");
+    var item = document.getElementById(input.value);
+    list.removeChild(item);
+}
